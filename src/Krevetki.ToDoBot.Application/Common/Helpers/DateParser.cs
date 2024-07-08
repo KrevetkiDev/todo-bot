cc29da973b7dd56db9f4a6a41ff65f1472b52800
Diana Agapova <d18102002@mail.ru>
2024-07-08 13:46:51 +0300

    [KREV-12] review fix

--- src/Krevetki.ToDoBot.Application/Common/Helpers/DateParser.cs
+++ src/Krevetki.ToDoBot.Application/Common/Helpers/DateParser.cs
@@ -4,15 +4,8 @@ public static class DateParser
 {
     public static bool TryParseDate(string inputMessage, out DateOnly dateList)
     {
-        var stringItems = inputMessage.Split(Messages.ListTasksByDateSignalSymbol);
-
-        if (stringItems.Length == 2 && DateOnly.TryParse(stringItems[1], out var date))
-        {
-            dateList = date;
-            return true;
-        }
-
-        dateList = default!;
-        return false;
+        dateList = default;
+        return inputMessage.StartsWith(Messages.ListTasksByDateSignalSymbol)
+               && DateOnly.TryParse(new string(inputMessage.Skip(1).ToArray()), out dateList);
     }
 }
